22bb57823ebf4eb2e36428313976ec26217e5cf4
cchin <Christopher.Chin@dt.com.au>
2014-02-18 15:29:38 +1100

    CC: Sitemap generator working

--- tourismaustralia-core/src/main/java/com/australia/utils/ServerUtils.java
+++ tourismaustralia-core/src/main/java/com/australia/utils/ServerUtils.java
@@ -5,4 +5,8 @@ public class ServerUtils {
 
 	public static final String AUTHOR= "author";
 	public static final String PUBLISH = "publish";
+
+    public static final String SITE_NAME_OZCOM = "australia";
+    public static final String SITE_NAME_FOODWINE = "food-and-wine";
+
 }

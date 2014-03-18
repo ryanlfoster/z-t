af2591ab3e32c5868f19b59ba93b870efa6d280e
Michael Hodgdon <mhodgdon@citytechinc.com>
2014-03-18 15:43:49 +1100

    fixed base path

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/footer/Footer.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/footer/Footer.java
@@ -10,21 +10,18 @@ import com.citytechinc.cq.component.annotations.Listener;
 /**
  *
  */
-@Component(group="Food and Wine", basePath="foodandwine/components/content/Footer",value = "Footer", listeners = {
+@Component(group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", value = "Footer", listeners = {
 	@Listener(name = "aftercopy", value = "REFRESH_PAGE"), @Listener(name = "afterdelete", value = "REFRESH_PAGE"),
 	@Listener(name = "afteredit", value = "REFRESH_PAGE"), @Listener(name = "afterinsert", value = "REFRESH_PAGE") })
 public class Footer {
 
 	@DialogField(fieldLabel = "Target Application", required = true)
-	private String applicationUrl;
-
-	
+	private final String applicationUrl;
 
 	public Footer(SlingHttpServletRequest request) {
 		ValueMap properties = request.getResource().adaptTo(ValueMap.class);
 		applicationUrl = properties.get("applicationUrl", "");
 	}
-
 
 	public String getApplicationUrl() {
 		return applicationUrl;

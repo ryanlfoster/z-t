5020bf60f028d5a3b08c5c3e7c28b905f6d2e533
wynnema <wynne.ma@dtdigital.com.au>
2014-03-18 15:28:34 +1100

    Implemented feedback from Internal Test

--- tourismaustralia-core/src/main/java/com/australia/www/components/content/imageWithDate/ImageWithDate.java
+++ tourismaustralia-core/src/main/java/com/australia/www/components/content/imageWithDate/ImageWithDate.java
@@ -1,58 +1,68 @@
-package com.australia.www.components.content.imageWithDate;
+package com.australia.www.components.content.imagewithdate;
 
 import com.citytechinc.cq.component.annotations.Tab;
-import com.citytechinc.cq.component.annotations.widgets.*;
-import com.citytechinc.cq.component.annotations.widgets.rte.Image;
+import com.citytechinc.cq.component.annotations.widgets.DateField;
+import com.citytechinc.cq.component.annotations.widgets.Html5SmartImage;
 import com.citytechinc.cq.component.annotations.widgets.RichTextEditor;
+import com.day.cq.wcm.foundation.Image;
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ValueMap;
-
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
-import org.omg.DynamicAny._DynFixedStub;
 
 /**
  * Created by wma on 12/03/14.
  */
-@Component(value = "Image with Date", tabs = {@Tab(title="Image"), @Tab(title="Extra Information")})
+@Component(value = "Image with Date", tabs = {@Tab(title = "Image"), @Tab(title = "Extra Information")})
 public class ImageWithDate {
 
-    @DialogField(fieldLabel = "Image", required = true, hideLabel = true, tab = 1 )
-    @Html5SmartImage(name="image",disableZoom = true, allowUpload = false, tab = true, height = 400 )
+    @DialogField(fieldLabel = "Image", required = true, hideLabel = true, tab = 1)
+    @Html5SmartImage(name = "image", disableZoom = true, allowUpload = false, tab = true, height = 400)
     private String image;
 
-    @DialogField(fieldLabel = "Alt Text", required = true, tab = 2 )
+    @DialogField(fieldLabel = "Alt Text", required = true, tab = 2)
     private String altText;
 
-    @DialogField(fieldLabel = "Date", required = false, tab = 2 )
-    @DateField( format = "M d", showToday = true)
+    @DialogField(fieldLabel = "Date", required = false, tab = 2)
+    @DateField(format = "M d", showToday = true)
     private String date;
 
-    @DialogField(fieldLabel = "Title", required = false, tab = 2 )
+    @DialogField(fieldLabel = "Title", required = false, tab = 2)
     private String title;
 
-    @DialogField(fieldLabel = "Text", required = false, tab = 2 )
-    @RichTextEditor()
+    @DialogField(fieldLabel = "Text", required = false, tab = 2)
+    @RichTextEditor
     private String text;
 
     public ImageWithDate(SlingHttpServletRequest request) {
         ValueMap properties = request.getResource().adaptTo(ValueMap.class);
-        altText = properties.get("altText", "");
-        date = properties.get("date", "");
-        title = properties.get("title", "");
-        text = properties.get("text", "");
-        com.day.cq.wcm.foundation.Image imageObj = new com.day.cq.wcm.foundation.Image(request.getResource(),"image");
-        image=imageObj.getSrc();
+        altText = properties.get("altText", StringUtils.EMPTY);
+        date = properties.get("date", StringUtils.EMPTY);
+        title = properties.get("title", StringUtils.EMPTY);
+        text = properties.get("text", StringUtils.EMPTY);
+        Image imageObj = new Image(request.getResource(), "image");
+        image = imageObj.getSrc();
     }
 
-    public String getImage() { return image; }
+    public String getImage() {
+        return image;
+    }
 
-    public String getAltText() { return altText; }
+    public String getAltText() {
+        return altText;
+    }
 
-    public String getDate() { return date; }
+    public String getDate() {
+        return date;
+    }
 
-    public String getTitle() { return title; }
+    public String getTitle() {
+        return title;
+    }
 
-    public String getText() { return text; }
+    public String getText() {
+        return text;
+    }
 
 }
--- tourismaustralia-ui/src/main/content/jcr_root/apps/tourismaustralia/components/content/imageWithDate/imageWithDate.jsp
+++ tourismaustralia-ui/src/main/content/jcr_root/apps/tourismaustralia/components/content/imageWithDate/imageWithDate.jsp
@@ -1,28 +1,28 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@include file="/apps/tourismaustralia/components/global.jsp" %>
-<%@ page import="com.australia.www.components.content.imageWithDate.ImageWithDate" %>
+<%@ page import="com.australia.www.components.content.imagewithdate.ImageWithDate" %>
 <%@ page import="com.day.cq.wcm.api.WCMMode" %>
 
 <c:set var="imageWithDate" value="<%=new ImageWithDate(slingRequest) %>"/>
 
 <% if (WCMMode.fromRequest(slingRequest) == WCMMode.EDIT) {%>
-    <c:if test="${not empty imageWithDate.image && not empty imageWithDate.altText }">
-        <div><img src='${imageWithDate.image}' alt="${imageWithDate.altText}" /></div>
-    </c:if>
+<c:if test="${not empty imageWithDate.image && not empty imageWithDate.altText }">
+    <div><img src='${imageWithDate.image}' alt="${imageWithDate.altText}"/></div>
+</c:if>
 
-    <c:if test="${not empty imageWithDate.date}">
-        <p>${imageWithDate.date}</p>
-    </c:if>
+<c:if test="${not empty imageWithDate.date}">
+    <p>${imageWithDate.date}</p>
+</c:if>
 
-    <c:if test="${not empty imageWithDate.title || not empty imageWithDate.text}">
-        <p>
-            <c:if test="${not empty imageWithDate.title}">
-                <strong>${imageWithDate.title}</strong><br />
-            </c:if>
+<c:if test="${not empty imageWithDate.title || not empty imageWithDate.text}">
+    <p>
+        <c:if test="${not empty imageWithDate.title}">
+            <strong>${imageWithDate.title}</strong><br/>
+        </c:if>
 
-            <c:if test="${not empty imageWithDate.text}">
-                ${imageWithDate.text}
-            </c:if>
-        </p>
-    </c:if>
+        <c:if test="${not empty imageWithDate.text}">
+            ${imageWithDate.text}
+        </c:if>
+    </p>
+</c:if>
 <% } %>

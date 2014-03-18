1307388a4c845a173e0c52631908aab7d7b82f8e
myeasmin <marjia.yeasmin@dt.com.au>
2014-03-18 11:03:49 +1100

    MY: Initital

---
+++ tourismaustralia-core/src/main/java/com/australia/www/components/content/imageWithButton/ImageWithButton.java
@@ -0,0 +1,14 @@
+package com.australia.www.components.content.imageWithButton;
+
+import com.citytechinc.cq.component.annotations.Component;
+import com.citytechinc.cq.component.annotations.Tab;
+
+/**
+ * Created by myeasmin on 17/03/14.
+ */
+@Component(value="Image With Button", tabs = {@Tab(title="Image"), @Tab(title="Text and Button")})
+public class ImageWithButton {
+    private static final String DEFAULT_TITLE="Default Image Title";
+
+    @Dialo
+}

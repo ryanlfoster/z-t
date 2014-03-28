c1425a7c3c2a79b9e8dd161e285b473294c990bb
Michael Hodgdon <mhodgdon@citytechinc.com>
2014-03-28 15:49:04 +1100

    added seperator

---
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/seperator/Seperator.java
@@ -0,0 +1,8 @@
+package com.australia.foodandwine.components.content.seperator;
+
+import com.citytechinc.cq.component.annotations.Component;
+
+@Component(actions = { "text:Seperator", "-" }, group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", value = "Seperator")
+public class Seperator {
+
+}

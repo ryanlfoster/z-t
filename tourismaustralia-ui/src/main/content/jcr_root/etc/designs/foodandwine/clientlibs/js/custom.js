92d94afdafe89ebb750efaa01a9c55d8ce2795e0
Michael Hodgdon <mhodgdon@citytechinc.com>
2014-03-28 15:37:39 +1100

    video hero fixes

--- tourismaustralia-ui/src/main/content/jcr_root/etc/designs/foodandwine/clientlibs/js/custom.js
+++ tourismaustralia-ui/src/main/content/jcr_root/etc/designs/foodandwine/clientlibs/js/custom.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
 	$(".video-container").video();
 });
+
+$(function(){
+	$('.faw-video-teaser-icon').click(function(){
+		var id=$(this).data('video-button')
+		$('#'+id).find('.BrightcoveExperience').each(function(){
+			 adjustHeightAndWidth(this);
+		});
+	});
+});
+
+$(function(){
+	$('.video-close-btn').click(function(){
+		$(this).parent().find('.BrightcoveExperience').each(function(){
+			var $this = $(this);
+		    var id = $this.attr("id");
+			var videoPlayerHolder = brightcove.api.getExperience(id);
+			var videoPlayer = videoPlayerHolder.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+			videoPlayer.pause();
+		});
+	});
+});

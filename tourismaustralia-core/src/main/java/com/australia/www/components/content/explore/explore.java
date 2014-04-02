93cdff9b30c709607248ba214fbc9f1e6140cfeb
Eduardo Antillon <eantillon@citytechinc.com>
2014-04-02 13:38:50 -0500

    Working on multi diolog fields.

--- tourismaustralia-core/src/main/java/com/australia/www/components/content/explore/explore.java
+++ tourismaustralia-core/src/main/java/com/australia/www/components/content/explore/explore.java
@@ -1,14 +1,20 @@
 package com.australia.www.components.content.explore;
 
 
+import com.australia.www.components.domain.Link;
 import com.citytechinc.cq.component.annotations.Component;
+import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.FieldProperty;
+
+import java.util.List;
 
 @Component(value = "Image Map with City Link" , disableTargeting = true)
 public class explore {
 
 
-
-
+	@DialogField(fieldLabel = "Categories")
+	@MultiField
+	@PathField
 
 
 

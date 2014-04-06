9c87db086e1193ac6a395258779a38b579670b21
Jamie Hayman <jamie@rkda.com.aum>
2014-04-06 23:51:22 +1000

    fixing responsive image bug

--- tourismaustralia-styleguide/assets/js/src/helper/responsive.image.helper.js
+++ tourismaustralia-styleguide/assets/js/src/helper/responsive.image.helper.js
@@ -6,32 +6,37 @@ var responsiveImageHelper = {};
 
 responsiveImageHelper.bindEvents = function(){
 
-	window.addEventListener("resize", function() {
+	window.addEventListener('resize', function() {
 	// Get screen size (inner/outerWidth, inner/outerHeight)
 		responsiveImageHelper.setContainerHeight();
 	}, false);
 
-	window.addEventListener("orientationchange", function() {
+	window.addEventListener('orientationchange', function() {
 		// Announce the new orientation number
 		responsiveImageHelper.setContainerHeight();
 	}, false);
+
+
+	responsiveImageHelper.setContainerHeight();
 
 };
 
 responsiveImageHelper.setContainerHeight = function(){
 	$('.largeimage-container').each(function(){
 		var offSet = 40,
-			maxImageHeight = 600 // Max height of responsive images
+			maxImageHeight = 600, // Max height of responsive images
 			heightOfImage = $(this).find('img').height() - offSet;
 
 		heightOfImage = ( heightOfImage <= maxImageHeight ) ? heightOfImage : maxImageHeight;
 
+		console.log(heightOfImage);
+
 		$(this).css('height', heightOfImage);
-		$(this).find('img').css('margin-top', -($(this).find('img').height() / 2));
+		$(this).find('img').css('margin-top', -($(this).find('img').height() / 2));	
+		
 	});
 };
 
-$(function(){
-	responsiveImageHelper.setContainerHeight();
+$(window).bind('load', function() {
 	responsiveImageHelper.bindEvents();
 });

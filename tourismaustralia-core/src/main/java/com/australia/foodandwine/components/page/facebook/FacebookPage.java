a8452bbc98f25160492b499b17540e921cf448fb
cchin <Christopher.Chin@dt.com.au>
2014-04-08 12:16:44 +1000

    FacebookPage page properties dialog now only displays for fb page templates

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/page/facebook/FacebookPage.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/page/facebook/FacebookPage.java
@@ -5,17 +5,19 @@ import org.apache.sling.api.resource.ValueMap;
 
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.widgets.PathField;
 
 @Component(group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", path = "page", value = "Facebook", fileName = "facebook_dialog")
 public class FacebookPage {
 
-	@DialogField(fieldLabel = "Username")
+	@DialogField(fieldLabel = "Username", required = true)
 	private String userName;
 
-	@DialogField(fieldLabel = "Post text")
+	@DialogField(fieldLabel = "Post Text", required = true)
 	private String postText;
 
-	@DialogField(fieldLabel = "Post link")
+	@DialogField(fieldLabel = "Post Link", required = true)
+	@PathField(rootPath = "/content")
 	private String postLink;
 
 	public FacebookPage(SlingHttpServletRequest request) {
--- tourismaustralia-ui/src/main/content/jcr_root/apps/foodandwine/components/page/facebookpage/content.jsp
+++ tourismaustralia-ui/src/main/content/jcr_root/apps/foodandwine/components/page/facebookpage/content.jsp
@@ -1,10 +1,12 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@include file="/apps/foodandwine/components/global.jsp"%>
 <%@ page import="com.australia.foodandwine.components.page.facebook.FacebookPage" %>
 
 <c:set var="facebook" value="<%=new FacebookPage(slingRequest) %>"/>
 
-<p>
-	${facebook.userName}<br/>
-	${facebook.postText}<br/>
-	${facebook.postLink}<br/>
-</p>
+<div>
+	<p>
+	<p>${facebook.userName}</p>
+	<p>${facebook.postText}</p>
+	<p>${facebook.postLink}</p>
+</div>

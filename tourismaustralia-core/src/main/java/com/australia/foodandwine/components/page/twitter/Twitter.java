1e446b4e77bc1ed38e1ca3ce7cca2af7f57f6f53
cchin <Christopher.Chin@dt.com.au>
2014-04-08 17:17:26 +1000

    Twitter.java page template added

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/page/twitter/Twitter.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/page/twitter/Twitter.java
@@ -1,4 +1,4 @@
-package com.australia.foodandwine.components.page.facebook;
+package com.australia.foodandwine.components.page.twitter;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ValueMap;
@@ -7,24 +7,24 @@ import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
 import com.citytechinc.cq.component.annotations.widgets.PathField;
 
-@Component(group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", path = "page", value = "Facebook", editConfig = false, fileName = "facebook_dialog")
-public class Facebook {
+@Component(group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", path = "page", value = "Twitter", editConfig = false, fileName = "twitter_dialog")
+public class Twitter {
 
-	@DialogField(fieldLabel = "Username", required = true)
+	@DialogField(fieldLabel = "Handle", required = true, fieldDescription = "Twitter handle e.g. @kimkardashian")
 	private String userName;
 
-	@DialogField(fieldLabel = "Post Text", required = true)
-	private String postText;
+	@DialogField(fieldLabel = "Tweet", required = true)
+	private String tweet;
 
 	@DialogField(fieldLabel = "Post Link", required = true)
 	@PathField(rootPath = "/content")
 	private String postLink;
 
-	public Facebook(SlingHttpServletRequest request) {
+	public Twitter(SlingHttpServletRequest request) {
 		ValueMap properties = request.getResource().adaptTo(ValueMap.class);
 		if (properties != null) {
 			this.userName = properties.get("userName", "");
-			this.postText = properties.get("postText", "");
+			this.tweet = properties.get("tweet", "");
 			this.postLink = properties.get("postLink", "");
 		}
 	}
@@ -33,8 +33,8 @@ public class Facebook {
 		return userName;
 	}
 
-	public String getPostText() {
-		return postText;
+	public String getTweet() {
+		return tweet;
 	}
 
 	public String getPostLink() {

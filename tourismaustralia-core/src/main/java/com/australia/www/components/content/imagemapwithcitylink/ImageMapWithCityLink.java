3e4bb9e81b47cf111e294f5b8fe85e85021d126f
wynnem <wynne.ma@dt.com>
2014-04-11 09:15:42 +1000

    WM: BugFix > Reformatted Java files

--- tourismaustralia-core/src/main/java/com/australia/www/components/content/imagemapwithcitylink/ImageMapWithCityLink.java
+++ tourismaustralia-core/src/main/java/com/australia/www/components/content/imagemapwithcitylink/ImageMapWithCityLink.java
@@ -17,66 +17,66 @@ import com.day.cq.wcm.foundation.Image;
  * Created by wma on 28/03/2014.
  */
 
-@Component(value = "Image Map with City Link", disableTargeting = true, tabs = { @Tab(title = "Image"),
-	@Tab(title = "Map Information") }, listeners = { @Listener(name = "aftercopy", value = "REFRESH_PAGE"),
-	@Listener(name = "afterdelete", value = "REFRESH_PAGE"), @Listener(name = "afteredit", value = "REFRESH_PAGE"),
-	@Listener(name = "afterinsert", value = "REFRESH_PAGE") })
+@Component(value = "Image Map with City Link", disableTargeting = true, tabs = {@Tab(title = "Image"),
+        @Tab(title = "Map Information")}, listeners = {@Listener(name = "aftercopy", value = "REFRESH_PAGE"),
+        @Listener(name = "afterdelete", value = "REFRESH_PAGE"), @Listener(name = "afteredit", value = "REFRESH_PAGE"),
+        @Listener(name = "afterinsert", value = "REFRESH_PAGE")})
 public class ImageMapWithCityLink {
-	@DialogField(fieldLabel = "Image", required = true, hideLabel = true, tab = 1)
-	@Html5SmartImage(name = "image", disableZoom = true, allowUpload = false, tab = true)
-	private String image;
+    @DialogField(fieldLabel = "Image", required = true, hideLabel = true, tab = 1)
+    @Html5SmartImage(name = "image", disableZoom = true, allowUpload = false, tab = true)
+    private String image;
 
-	@DialogField(fieldLabel = "Alt Text", required = true, tab = 2)
-	private String altText;
+    @DialogField(fieldLabel = "Alt Text", required = true, tab = 2)
+    private String altText;
 
-	@DialogField(fieldLabel = "Title", required = false, tab = 2)
-	private String title;
+    @DialogField(fieldLabel = "Title", required = false, tab = 2)
+    private String title;
 
-	@DialogField(fieldLabel = "Link Text", required = false, tab = 2)
-	private String linkText;
+    @DialogField(fieldLabel = "Link Text", required = false, tab = 2)
+    private String linkText;
 
-	@DialogField(fieldLabel = "Link", fieldDescription = "For external links please use prefix http:// or https:// (eg. http://www.google.com)", required = false, tab = 2)
-	@PathField
-	private String link;
+    @DialogField(fieldLabel = "Link", fieldDescription = "For external links please use prefix http:// or https:// (eg. http://www.google.com)", required = false, tab = 2)
+    @PathField
+    private String link;
 
-	private boolean linkIsExternal;
+    private boolean linkIsExternal;
 
-	public ImageMapWithCityLink(SlingHttpServletRequest request) {
-		ValueMap properties = request.getResource().adaptTo(ValueMap.class);
-		Image imageObj = new Image(request.getResource(), "image");
-		if (imageObj != null && imageObj.hasContent()) {
-			image = imageObj.getPath();
-		}
-		if (properties != null) {
-			altText = properties.get("altText", StringUtils.EMPTY);
-			title = properties.get("title", StringUtils.EMPTY);
-			linkText = properties.get("linkText", StringUtils.EMPTY);
-			link = LinkUtils.getHrefFromPath(properties.get("link", StringUtils.EMPTY));
-			linkIsExternal = LinkUtils.isExternal(properties.get("link", StringUtils.EMPTY));
-		}
-	}
+    public ImageMapWithCityLink(SlingHttpServletRequest request) {
+        ValueMap properties = request.getResource().adaptTo(ValueMap.class);
+        Image imageObj = new Image(request.getResource(), "image");
+        if (imageObj != null && imageObj.hasContent()) {
+            image = imageObj.getPath();
+        }
+        if (properties != null) {
+            altText = properties.get("altText", StringUtils.EMPTY);
+            title = properties.get("title", StringUtils.EMPTY);
+            linkText = properties.get("linkText", StringUtils.EMPTY);
+            link = LinkUtils.getHrefFromPath(properties.get("link", StringUtils.EMPTY));
+            linkIsExternal = LinkUtils.isExternal(properties.get("link", StringUtils.EMPTY));
+        }
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage() {
+        return image;
+    }
 
-	public String getAltText() {
-		return altText;
-	}
+    public String getAltText() {
+        return altText;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getLinkText() {
-		return linkText;
-	}
+    public String getLinkText() {
+        return linkText;
+    }
 
-	public String getLink() {
-		return link;
-	}
+    public String getLink() {
+        return link;
+    }
 
-	public boolean getLinkIsExternal() {
-		return linkIsExternal;
-	}
+    public boolean getLinkIsExternal() {
+        return linkIsExternal;
+    }
 }

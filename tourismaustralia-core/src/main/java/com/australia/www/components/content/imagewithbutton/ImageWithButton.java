d1c22d0b731ca3da6a7c2a1af8218b8a075cf235
myeasmin <marjia.yeasmin@dt.com.au>
2014-04-10 14:29:11 +1000

    MY: BugFix #75197, #75200, #75201, #75208 > Worked around the issues on "large image with overlays and reference" component.

--- tourismaustralia-core/src/main/java/com/australia/www/components/content/imagewithbutton/ImageWithButton.java
+++ tourismaustralia-core/src/main/java/com/australia/www/components/content/imagewithbutton/ImageWithButton.java
@@ -1,5 +1,8 @@
 package com.australia.www.components.content.imagewithbutton;
 
+import com.australia.utils.LinkUtils;
+import com.australia.www.components.content.links.Links;
+import com.citytechinc.cq.component.annotations.*;
 import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ValueMap;
@@ -13,30 +16,33 @@ import com.day.cq.wcm.foundation.Image;
 
 /**
  * Created by myeasmin on 18/03/14.
- * 
- * This component is composed of 4 tabs - Main Image, Overlay Icon Image,
- * Reference Icon Image, and Additional Properties.
- */
-@Component(value = "Image With Button", disableTargeting = true, tabs = { @Tab(title = "Main Image"),
-	@Tab(title = "Overlay Icon Image"), @Tab(title = "Reference Icon Image"), @Tab(title = "Additional Properties") })
+ *
+ * This component is composed of 4 tabs - Main Image, Overlay Icon Image, Reference Icon Image, and Additional Properties.
+  */
+@Component(value="Image With Button", disableTargeting = true , tabs = {@Tab(title="Main Image"), @Tab(title="Overlay Icon Image"), @Tab(title="Reference Icon Image"), @Tab(title="Additional Properties")}, listeners = {
+	@Listener(name = "afteredit", value = "REFRESH_PAGE"),
+	@Listener(name = "aftercopy", value = "REFRESH_PAGE"),
+	@Listener(name = "afterdelete", value = "REFRESH_PAGE"),
+	@Listener(name = "afterinsert", value = "REFRESH_PAGE")})
+
 public class ImageWithButton {
 
 	@DialogField(required = true, tab = 1, hideLabel = true)
-	@Html5SmartImage(height = 400, tab = false, name = "mainImageSrc")
+	@Html5SmartImage(height = 400, tab = false, name="mainImageSrc")
 	private final String mainImageSrc;
 
 	@DialogField(required = true, tab = 1, fieldLabel = "Main Image Alt")
 	private final String mainImageAlt;
 
 	@DialogField(tab = 2, hideLabel = true)
-	@Html5SmartImage(height = 400, tab = false, name = "overlayIconImageSrc")
+	@Html5SmartImage(height = 400, tab = false, name="overlayIconImageSrc")
 	private final String overlayIconImageSrc;
 
 	@DialogField(tab = 2, fieldLabel = "Overlay Icon Image Alt")
 	private final String overlayIconImageAlt;
 
 	@DialogField(tab = 3, hideLabel = true)
-	@Html5SmartImage(height = 400, tab = false, name = "refIconImageSrc")
+	@Html5SmartImage(height = 400, tab = false, name="refIconImageSrc")
 	private final String refIconImageSrc;
 
 	@DialogField(tab = 3, fieldLabel = " Reference Icon Image Alt")
@@ -49,32 +55,35 @@ public class ImageWithButton {
 	@PathField
 	private String imageButtonPath;
 
+	private boolean buttonPathExternal;
+
 	@DialogField(tab = 4, fieldLabel = "Overlay Quote Text")
 	private final String quoteText;
 
 	@DialogField(tab = 4, fieldLabel = "Reference Bold Text")
-	private final String refBoldText;
+    private final String refBoldText;
 
-	@DialogField(tab = 4, fieldLabel = "Reference Regular Text")
-	private final String refRegularText;
+    @DialogField(tab = 4, fieldLabel = "Reference Regular Text")
+    private final String refRegularText;
 
 	public ImageWithButton(SlingHttpServletRequest slingRequest) {
 		ValueMap properties = slingRequest.getResource().adaptTo(ValueMap.class);
 
-		Image mainImage = new Image(slingRequest.getResource(), "mainImageSrc");
+		Image mainImage = new Image(slingRequest.getResource(),"mainImageSrc");
 		this.mainImageSrc = mainImage.getSrc();
 		this.mainImageAlt = properties.get("mainImageAlt", StringUtils.EMPTY);
 
-		Image overlayIconImage = new Image(slingRequest.getResource(), "overlayIconImageSrc");
+		Image overlayIconImage = new Image(slingRequest.getResource(),"overlayIconImageSrc");
 		this.overlayIconImageSrc = overlayIconImage.getSrc();
 		this.overlayIconImageAlt = properties.get("overlayIconImageAlt", StringUtils.EMPTY);
 
-		Image refIconImage = new Image(slingRequest.getResource(), "refIconImageSrc");
+		Image refIconImage = new Image(slingRequest.getResource(),"refIconImageSrc");
 		this.refIconImageSrc = refIconImage.getSrc();
 		this.refIconImageAlt = properties.get("refIconImageAlt", StringUtils.EMPTY);
 
 		this.imageButtonText = properties.get("imageButtonText", StringUtils.EMPTY);
-		setImageButtonPath(properties.get("imageButtonPath", StringUtils.EMPTY));
+		this.imageButtonPath = LinkUtils.getHrefFromPath(properties.get("imageButtonPath", StringUtils.EMPTY));
+		this.buttonPathExternal = LinkUtils.isExternal(properties.get("imageButtonPath", StringUtils.EMPTY));
 
 		this.quoteText = properties.get("quoteText", StringUtils.EMPTY);
 		this.refBoldText = properties.get("refBoldText", StringUtils.EMPTY);
@@ -83,12 +92,12 @@ public class ImageWithButton {
 	}
 
 	public String getMainImageSrc() {
-		return mainImageSrc;
-	}
+        return mainImageSrc;
+    }
 
-	public String getMainImageAlt() {
-		return mainImageAlt;
-	}
+    public String getMainImageAlt() {
+        return mainImageAlt;
+    }
 
 	public String getOverlayIconImageSrc() {
 		return overlayIconImageSrc;
@@ -110,28 +119,22 @@ public class ImageWithButton {
 		return imageButtonText;
 	}
 
-	public String getImageButtonPath() {
-		return imageButtonPath;
+	public String getImageButtonPath() { return imageButtonPath; }
+
+	public boolean isButtonPathExternal() {
+		return buttonPathExternal;
 	}
 
 	public String getQuoteText() {
-		return quoteText;
-	}
+        return quoteText;
+    }
 
-	public String getRefBoldText() {
-		return refBoldText;
-	}
+    public String getRefBoldText() {
+        return refBoldText;
+    }
 
-	public String getRefRegularText() {
-		return refRegularText;
-	}
-
-	private void setImageButtonPath(String buttonPath) {
-		if (!(StringUtils.isEmpty(buttonPath))) {
-			imageButtonPath = buttonPath + ".html";
-		} else {
-			imageButtonPath = "";
-		}
-	}
+    public String getRefRegularText() {
+        return refRegularText;
+    }
 
 }

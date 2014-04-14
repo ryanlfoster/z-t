83965612cbe39487b3400161e815129478cb96f8
Michael Hodgdon <mhodgdon@citytechinc.com>
2014-04-14 14:30:38 +1000

    moved to a service and clean up

--- tourismaustralia-core/src/main/java/com/australia/cqimport/service/IconBuilder.java
+++ tourismaustralia-core/src/main/java/com/australia/cqimport/service/IconBuilder.java
@@ -1,77 +1,82 @@
 package com.australia.cqimport.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.sling.api.resource.PersistenceException;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.jcr.resource.JcrResourceConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.australia.cqimport.domain.ConsumerBaseFoundationType;
 import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.WCMException;
-import org.apache.sling.api.resource.PersistenceException;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.jcr.resource.JcrResourceConstants;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Viren Pushpanayagam on 1/04/2014.
  */
 public class IconBuilder extends PageBuilder {
+	private static final Logger LOG = LoggerFactory.getLogger(IconBuilder.class);
+	private static final String TEMPLATE = "/apps/tourismaustralia/templates/cityState";
 
-    private static final String TEMPLATE = "/apps/tourismaustralia/templates/cityState";
+	@Override
+	public void createPage(String oldPath, String newPath, ResourceResolver resourceResolver, boolean addMixin)
+		throws WCMException {
+		PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+		Page page = pageManager.getPage(PageBuilder.CRX_ROOT_PATH + newPath);
 
-    @Override
-    public void createPage(String oldPath, String newPath, ResourceResolver resourceResolver, boolean addMixin) throws WCMException {
-        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
-        Page page = pageManager.getPage(PageBuilder.CRX_ROOT_PATH + newPath);
+		if (page == null) {
+			LOG.debug("Importing: " + PageBuilder.CRX_ROOT_PATH + oldPath);
+			DefaultConsumerBaseFoundationService service = new DefaultConsumerBaseFoundationService();
+			ConsumerBaseFoundationType cbf = service.getByPath(oldPath);
 
-        if (page == null) {
-            System.out.println("Importing: "+ PageBuilder.CRX_ROOT_PATH + oldPath);
-            DefaultConsumerBaseFoundationService service = new DefaultConsumerBaseFoundationService();
-            ConsumerBaseFoundationType cbf = service.getByPath(oldPath);
+			page = pageManager.create(getPath(PageBuilder.CRX_ROOT_PATH + newPath, false),
+				getPath(PageBuilder.CRX_ROOT_PATH + newPath, true), TEMPLATE, null, true);
 
-            page = pageManager.create(getPath(PageBuilder.CRX_ROOT_PATH + newPath, false), getPath(PageBuilder.CRX_ROOT_PATH
-                    + newPath, true), TEMPLATE, null, true);
+			createHero(page, resourceResolver, cbf);
+			createSummery(page, resourceResolver, cbf);
+			// LOG.debug(cbf.getHdlTitle());
 
-            createHero(page,resourceResolver,cbf);
-            createSummery(page,resourceResolver,cbf);
-            //System.out.println(cbf.getHdlTitle());
+		}
+	}
 
-        }
-    }
+	private void createHero(Page page, ResourceResolver resourceResolver, ConsumerBaseFoundationType cbf) {
+		Resource pageResource = page.adaptTo(Resource.class);
+		Resource jcrContentResource = pageResource.getChild(JcrConstants.JCR_CONTENT);
+		try {
 
-    private void createHero(Page page, ResourceResolver resourceResolver, ConsumerBaseFoundationType cbf) {
-        Resource pageResource = page.adaptTo(Resource.class);
-        Resource jcrContentResource = pageResource.getChild(JcrConstants.JCR_CONTENT);
-        try {
+			Map params = new HashMap<String, String>();
+			params.put("title", cbf.getHdlTitle());
+			params.put("image", cbf.getImgBackground());
+			params.put(JcrResourceConstants.SLING_RESOURCE_TYPE_PROPERTY, "tourismaustralia/components/content/hero");
 
-            Map params = new HashMap<String,String>();
-            params.put("title",cbf.getHdlTitle());
-            params.put("image",cbf.getImgBackground());
-            params.put(JcrResourceConstants.SLING_RESOURCE_TYPE_PROPERTY,"tourismaustralia/components/content/hero");
+			resourceResolver.create(jcrContentResource, "hero", params);
+			resourceResolver.commit();
+		} catch (PersistenceException e) {
+			LOG.error(e.getMessage(), e);
+		}
 
-            resourceResolver.create(jcrContentResource, "hero", params);
-            resourceResolver.commit();
-        } catch (PersistenceException e) {
-            e.printStackTrace();
-        }
+	}
 
-    }
+	private void createSummery(Page page, ResourceResolver resourceResolver, ConsumerBaseFoundationType cbf) {
+		Resource pageResource = page.adaptTo(Resource.class);
+		Resource jcrContentResource = pageResource.getChild(JcrConstants.JCR_CONTENT);
+		try {
 
-    private void createSummery(Page page, ResourceResolver resourceResolver, ConsumerBaseFoundationType cbf) {
-        Resource pageResource = page.adaptTo(Resource.class);
-        Resource jcrContentResource = pageResource.getChild(JcrConstants.JCR_CONTENT);
-        try {
+			Map params = new HashMap<String, String>();
+			params.put("text", cbf.getStfDescription());
+			params
+				.put(JcrResourceConstants.SLING_RESOURCE_TYPE_PROPERTY, "tourismaustralia/components/content/summery");
 
-            Map params = new HashMap<String,String>();
-            params.put("text",cbf.getStfDescription());
-            params.put(JcrResourceConstants.SLING_RESOURCE_TYPE_PROPERTY,"tourismaustralia/components/content/summery");
+			resourceResolver.create(jcrContentResource, "summary", params);
+			resourceResolver.commit();
+		} catch (PersistenceException e) {
+			LOG.error(e.getMessage(), e);
+		}
 
-            resourceResolver.create(jcrContentResource, "summary", params);
-            resourceResolver.commit();
-        } catch (PersistenceException e) {
-            e.printStackTrace();
-        }
-
-    }
+	}
 }
--- tourismaustralia-core/src/main/java/com/australia/cqimport/servlet/ContentImporter.java
+++ tourismaustralia-core/src/main/java/com/australia/cqimport/servlet/ContentImporter.java
@@ -1,14 +1,6 @@
 package com.australia.cqimport.servlet;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 
@@ -18,18 +10,9 @@ import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.LoginException;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 
-import com.australia.cqimport.service.ArticleBuilder;
-import com.australia.cqimport.service.IconBuilder;
-import com.australia.cqimport.service.PageBuilder;
-import com.australia.cqimport.service.StateCityBuilder;
-import com.australia.cqimport.vo.MappingVO;
-import com.day.cq.wcm.api.WCMException;
-import com.google.common.collect.ImmutableMap;
+import com.australia.cqimport.service.ContentImporterService;
 
 /**
  * Created by Viren Pushpanayagam on 27/03/2014.
@@ -51,114 +34,13 @@ public class ContentImporter extends SlingAllMethodsServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Reference
-	private ResourceResolverFactory resourceResolverFactory;
-	Map<String, PageBuilder> templates = ImmutableMap.<String, PageBuilder> builder()
-		.put("state", new StateCityBuilder()).put("city", new StateCityBuilder()).put("article", new ArticleBuilder())
-		.put("icon", new IconBuilder()).build();
-	Map<String, String> languages = ImmutableMap.<String, String> builder().put("en", "csv/mapping_en.csv")
-		.put("it", "csv/mapping_it.csv").put("fr", "csv/mapping_fr.csv").put("de", "csv/mapping_de.csv")
-		.put("es", "csv/mapping_es.csv").put("id", "csv/mapping_id.csv").put("jp", "csv/mapping_jp.csv")
-		.put("ko", "csv/mapping_ko.csv").put("my", "csv/mapping_my.csv").put("pt", "csv/mapping_pt.csv")
-		.put("zht", "csv/mapping_zht.csv").build();
-
-	@Override
-	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException,
-		IOException {
-		doPost(request, response);
-	}
+	private ContentImporterService contentImporter;
 
 	@Override
 	protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException,
 		IOException {
-		ResourceResolver resourceResolver = null;
-
 		String lang = request.getParameter("lang");
-		boolean addMixin = false;
-
-		lang = (lang != null) ? lang.trim().toLowerCase() : "en";
-
-		if (!"en".equals(lang)) {
-			addMixin = true;
-		}
-
-		try {
-			resourceResolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
-		} catch (LoginException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Start Importing *************************");
-
-		PageBuilder pageBuilder;
-
-		ArrayList<MappingVO> mappingVO = getMappings(lang);
-
-		Collections.sort(mappingVO, new Comparator<MappingVO>() {
-			@Override
-			public int compare(MappingVO obj1, MappingVO obj2) {
-				return obj1.getNewPath().compareTo(obj2.getNewPath());
-			}
-		});
-
-		for (MappingVO map : mappingVO) {
-			// System.out.println(map.getNewPath());
-			pageBuilder = templates.get(map.getTemplate());
-
-			if (pageBuilder == null) {
-				continue;
-			}
-
-			try {
-
-				pageBuilder.createPage(map.getOldPath(), map.getNewPath(), resourceResolver, addMixin);
-
-			} catch (WCMException e) {
-				System.out.println(e);
-				e.printStackTrace();
-			} catch (Exception e) {
-				System.out.println(e);
-			}
-
-		}
-
-		System.out.println("Importing Finished *************************");
-
-	}
-
-	private ArrayList<MappingVO> getMappings(String lang) {
-
-		InputStream file = this.getClass().getClassLoader().getResourceAsStream(languages.get(lang));
-		BufferedReader br = null;
-		String line;
-		String cvsSplitBy = ",";
-		ArrayList<MappingVO> mappingsList = new ArrayList<MappingVO>();
-		try {
-
-			br = new BufferedReader(new InputStreamReader(file));
-			while ((line = br.readLine()) != null) {
-				MappingVO mappingVO = new MappingVO();
-				// use comma as separator
-				String[] items = line.split(cvsSplitBy);
-				mappingVO.setOldPath(items[2]);
-				mappingVO.setNewPath(items[1]);
-				mappingVO.setTemplate(items[0]);
-
-				mappingsList.add(mappingVO);
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return mappingsList;
+		contentImporter.importLanguage(lang);
 
 	}
 }

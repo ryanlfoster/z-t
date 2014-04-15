310a5cc226cd96ef0ec52af09149051fa99536f5
cchin <Christopher.Chin@dt.com.au>
2014-04-15 11:01:24 +1000

    state item object names changed to match css class names

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/carouselState/CarouselState.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/carouselState/CarouselState.java
@@ -13,7 +13,7 @@ import com.citytechinc.cq.component.annotations.widgets.DialogFieldSet;
 import com.citytechinc.cq.component.annotations.widgets.Html5SmartImage;
 import com.day.cq.wcm.foundation.Image;
 
-@Component(value = "State Carousel", group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", dialogHeight = 600, disableTargeting = true, tabs = {
+@Component(value = "Carousel State", group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", dialogHeight = 600, disableTargeting = true, tabs = {
 	@Tab(title = "General"), @Tab(title = "ACT"), @Tab(title = "NSW"), @Tab(title = "NT"), @Tab(title = "QLD"),
 	@Tab(title = "SA"), @Tab(title = "TAS"), @Tab(title = "VIC"), @Tab(title = "WA") }, listeners = {
 	@Listener(name = "aftercopy", value = "REFRESH_PAGE"), @Listener(name = "afterdelete", value = "REFRESH_PAGE"),
@@ -37,36 +37,36 @@ public class CarouselState {
 	private String imagePath;
 
 	@DialogField(tab = 2)
-	@DialogFieldSet(border = false, collapsible = false, namePrefix = "act/")
-	private final StateItem act;
+	@DialogFieldSet(border = false, collapsible = false, namePrefix = "capital/")
+	private final StateItem capital;
 
 	@DialogField(tab = 3)
 	@DialogFieldSet(border = false, collapsible = false, namePrefix = "nsw/")
 	private final StateItem nsw;
 
 	@DialogField(tab = 4)
-	@DialogFieldSet(border = false, collapsible = false, namePrefix = "nt/")
-	private final StateItem nt;
+	@DialogFieldSet(border = false, collapsible = false, namePrefix = "northern/")
+	private final StateItem northern;
 
 	@DialogField(tab = 5)
-	@DialogFieldSet(border = false, collapsible = false, namePrefix = "qld/")
-	private final StateItem qld;
+	@DialogFieldSet(border = false, collapsible = false, namePrefix = "queensland/")
+	private final StateItem queensland;
 
 	@DialogField(tab = 6)
-	@DialogFieldSet(border = false, collapsible = false, namePrefix = "sa/")
-	private final StateItem sa;
+	@DialogFieldSet(border = false, collapsible = false, namePrefix = "south/")
+	private final StateItem south;
 
 	@DialogField(tab = 7)
-	@DialogFieldSet(border = false, collapsible = false, namePrefix = "tas/")
-	private final StateItem tas;
+	@DialogFieldSet(border = false, collapsible = false, namePrefix = "tasmania/")
+	private final StateItem tasmania;
 
 	@DialogField(tab = 8)
-	@DialogFieldSet(border = false, collapsible = false, namePrefix = "vic/")
-	private final StateItem vic;
+	@DialogFieldSet(border = false, collapsible = false, namePrefix = "victoria/")
+	private final StateItem victoria;
 
 	@DialogField(tab = 9)
-	@DialogFieldSet(border = false, collapsible = false, namePrefix = "wa/")
-	private final StateItem wa;
+	@DialogFieldSet(border = false, collapsible = false, namePrefix = "western/")
+	private final StateItem western;
 
 	public CarouselState(SlingHttpServletRequest request) {
 		Resource resource = request.getResource();
@@ -80,14 +80,14 @@ public class CarouselState {
 		circleMiddleText = properties.get("circleMiddleText", StringUtils.EMPTY);
 		circleBottomText = properties.get("circleBottomText", StringUtils.EMPTY);
 
-		this.act = buildStateItem(resource.getChild("act"));
+		this.capital = buildStateItem(resource.getChild("capital"));
 		this.nsw = buildStateItem(resource.getChild("nsw"));
-		this.nt = buildStateItem(resource.getChild("nt"));
-		this.qld = buildStateItem(resource.getChild("qld"));
-		this.sa = buildStateItem(resource.getChild("sa"));
-		this.tas = buildStateItem(resource.getChild("tas"));
-		this.vic = buildStateItem(resource.getChild("vic"));
-		this.wa = buildStateItem(resource.getChild("wa"));
+		this.northern = buildStateItem(resource.getChild("northern"));
+		this.queensland = buildStateItem(resource.getChild("queensland"));
+		this.south = buildStateItem(resource.getChild("south"));
+		this.tasmania = buildStateItem(resource.getChild("tasmania"));
+		this.victoria = buildStateItem(resource.getChild("victoria"));
+		this.western = buildStateItem(resource.getChild("western"));
 	}
 
 	private StateItem buildStateItem(Resource resource) {
@@ -114,36 +114,36 @@ public class CarouselState {
 		return circleBottomText;
 	}
 
-	public StateItem getAct() {
-		return act;
+	public StateItem getCapital() {
+		return capital;
 	}
 
 	public StateItem getNsw() {
 		return nsw;
 	}
 
-	public StateItem getNt() {
-		return nt;
+	public StateItem getNorthern() {
+		return northern;
 	}
 
-	public StateItem getQld() {
-		return qld;
+	public StateItem getQueensland() {
+		return queensland;
 	}
 
-	public StateItem getSa() {
-		return sa;
+	public StateItem getSouth() {
+		return south;
 	}
 
-	public StateItem getTas() {
-		return tas;
+	public StateItem getTasmania() {
+		return tasmania;
 	}
 
-	public StateItem getVic() {
-		return vic;
+	public StateItem getVictoria() {
+		return victoria;
 	}
 
-	public StateItem getWa() {
-		return wa;
+	public StateItem getWestern() {
+		return western;
 	}
 
 }

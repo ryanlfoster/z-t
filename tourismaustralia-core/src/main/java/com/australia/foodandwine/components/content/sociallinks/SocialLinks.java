a5c4148dc4a80772efba60b6936ac0b2305f9bf2
mamshad <mamshad@Apurwas-MacBook-Pro.local>
2014-04-16 10:27:26 +0530

    Modified SocialLinks component

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/sociallinks/SocialLinks.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/sociallinks/SocialLinks.java
@@ -7,6 +7,7 @@ import org.apache.sling.api.resource.ValueMap;
 
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.FieldProperty;
 import com.citytechinc.cq.component.annotations.Listener;
 import com.citytechinc.cq.component.annotations.Tab;
 import com.citytechinc.cq.component.annotations.widgets.DialogFieldSet;
@@ -16,26 +17,32 @@ import com.citytechinc.cq.component.annotations.widgets.DialogFieldSet;
  * 
  */
 @Component(disableTargeting = true, group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", value = "Social Links", tabs = {
-	@Tab(title = "Social Links 1"), @Tab(title = "Social Links 2"), @Tab(title = "Social Links 3") }, listeners = {
+	@Tab(title="Main title"),@Tab(title = "Social Links 1"), @Tab(title = "Social Links 2"), @Tab(title = "Social Links 3") }, listeners = {
 	@Listener(name = "aftercopy", value = "REFRESH_PAGE"), @Listener(name = "afterdelete", value = "REFRESH_PAGE"),
 	@Listener(name = "afteredit", value = "REFRESH_PAGE"), @Listener(name = "afterinsert", value = "REFRESH_PAGE") })
 public class SocialLinks {
+	
 
-	@DialogField(tab = 1)
+	@DialogField(fieldLabel="Title",additionalProperties = @FieldProperty(name = "maxLength", value = "30"))
+	private String title;
+	
+
+	@DialogField(tab = 2)
 	@DialogFieldSet(border = false, collapsible = false, namePrefix = "socialLink1/")
 	private final SocialLinksProperties socialLink1;
 
-	@DialogField(tab = 2)
+	@DialogField(tab = 3)
 	@DialogFieldSet(border = false, collapsible = false, namePrefix = "socialLink2/")
 	private final SocialLinksProperties socialLink2;
 
-	@DialogField(tab = 3)
+	@DialogField(tab = 4)
 	@DialogFieldSet(border = false, collapsible = false, namePrefix = "socialLink3/")
 	private final SocialLinksProperties socialLink3;
 
 	/**
 	 * constants
 	 */
+	private static final String TITLE = "title";
 	private static final String SOCIAL_LINKS_LOGO = "socialLinksLogoPath";
 	private static final String SOCIAL_LINKS_ALT_TAG = "socialLinksAltTag";
 	private static final String SOCIAL_LINKS_TITLE = "socialLinksTitle";
@@ -66,7 +73,9 @@ public class SocialLinks {
 		}
 		return null;
 	}
-
+	public String getTitle() {
+		return title;
+	}
 	public SocialLinksProperties getSocialLink1() {
 		return socialLink1;
 	}

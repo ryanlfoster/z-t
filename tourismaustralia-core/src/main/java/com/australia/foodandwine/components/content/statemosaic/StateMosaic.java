8567f11d1a0c811fc33b4c242bfe93c6d3c9c06d
Michael Hodgdon <mhodgdon@citytechinc.com>
2014-04-17 11:39:17 +1000

    more todos

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/statemosaic/StateMosaic.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/statemosaic/StateMosaic.java
@@ -1,7 +1,5 @@
 package com.australia.foodandwine.components.content.statemosaic;
 
-import javax.servlet.http.HttpSession;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ValueMap;
@@ -12,35 +10,34 @@ import com.citytechinc.cq.component.annotations.Listener;
 import com.citytechinc.cq.component.annotations.Tab;
 import com.citytechinc.cq.component.annotations.widgets.TagInputField;
 
-@Component(disableTargeting = true, group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", value = "State Mosaic", tabs={
-		@Tab(title="Heading Text"),@Tab(title="Tag")},
-		listeners = {
-		@Listener(name = "aftercopy", value = "REFRESH_PAGE"), @Listener(name = "afterdelete", value = "REFRESH_PAGE"),
-		@Listener(name = "afteredit", value = "REFRESH_PAGE"), @Listener(name = "afterinsert", value = "REFRESH_PAGE") })
+@Component(disableTargeting = true, group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", value = "State Mosaic", tabs = {
+	@Tab(title = "Heading Text"), @Tab(title = "Tag") }, listeners = {
+	@Listener(name = "aftercopy", value = "REFRESH_PAGE"), @Listener(name = "afterdelete", value = "REFRESH_PAGE"),
+	@Listener(name = "afteredit", value = "REFRESH_PAGE"), @Listener(name = "afterinsert", value = "REFRESH_PAGE") })
 public class StateMosaic {
-	
-	@DialogField(fieldLabel="Top Text",tab=1)
-	private String topText;
-	
-	@DialogField(fieldLabel="Center Text",tab=1)
-	private String centerText;
-	
-	@DialogField(fieldLabel="Bottom text", tab=1)
-	private String bottomText;
-	
-	@DialogField(fieldLabel="Select", tab=2)
+
+	@DialogField(fieldLabel = "Top Text", tab = 1)
+	private final String topText;
+
+	@DialogField(fieldLabel = "Center Text", tab = 1)
+	private final String centerText;
+
+	@DialogField(fieldLabel = "Bottom text", tab = 1)
+	private final String bottomText;
+
+	@DialogField(fieldLabel = "Select", tab = 2)
 	@TagInputField()
-	private String select;
-	
-	public StateMosaic(SlingHttpServletRequest request)
-	{
-		ValueMap properties=request.getResource().adaptTo(ValueMap.class);
-		select=properties.get("select",StringUtils.EMPTY);
-		topText=properties.get("topText",StringUtils.EMPTY);
-		centerText=properties.get("centerText",StringUtils.EMPTY);
-		bottomText=properties.get("bottomText",StringUtils.EMPTY);
+	private final String select;
+
+	public StateMosaic(SlingHttpServletRequest request) {
+		// TODO: Load first set server side so it can be indexed by google
+		ValueMap properties = request.getResource().adaptTo(ValueMap.class);
+		select = properties.get("select", StringUtils.EMPTY);
+		topText = properties.get("topText", StringUtils.EMPTY);
+		centerText = properties.get("centerText", StringUtils.EMPTY);
+		bottomText = properties.get("bottomText", StringUtils.EMPTY);
 	}
-	
+
 	public String getSelect() {
 		return select;
 	}
@@ -56,6 +53,5 @@ public class StateMosaic {
 	public String getBottomText() {
 		return bottomText;
 	}
-	
 
 }

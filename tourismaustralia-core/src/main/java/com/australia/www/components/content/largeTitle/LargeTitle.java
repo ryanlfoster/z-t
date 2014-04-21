96ff4e015262dda9e4b03030e20f65096017450d
Marc Moylan <mmoyla2@uic.edu>
2014-04-21 13:59:29 -0500

    backing bean stubbed out

---
+++ tourismaustralia-core/src/main/java/com/australia/www/components/content/largeTitle/LargeTitle.java
@@ -0,0 +1,27 @@
+package com.australia.www.components.content.largeTitle;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.ValueMap;
+
+import com.citytechinc.cq.component.annotations.Component;
+import com.citytechinc.cq.component.annotations.DialogField;
+
+@Component(value = "Large Title")
+public class LargeTitle {
+
+	@DialogField
+	private String title;
+
+	public LargeTitle(SlingHttpServletRequest request) {
+		ValueMap properties = request.getResource().adaptTo(ValueMap.class);
+		if (properties != null) {
+			title = properties.get("title", StringUtils.EMPTY);
+		}
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+}

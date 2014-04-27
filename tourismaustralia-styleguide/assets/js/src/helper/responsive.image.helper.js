1c594c893c4cbe27b8d8b53ed3be78906ef7baff
Jamie Hayman <jamie@rkda.com.aum>
2014-04-27 23:33:46 +1000

    bug fixing and small edits

--- tourismaustralia-styleguide/assets/js/src/helper/responsive.image.helper.js
+++ tourismaustralia-styleguide/assets/js/src/helper/responsive.image.helper.js
@@ -44,25 +44,5 @@ responsiveImageHelper.setContainerHeight = function(){
 };
 
 $(window).bind('load', function() {
-	responsiveImageHelper.bindEvents();
-
-
-
-	// Used for new parallax
-	var touch = Modernizr.touch,
-		csstransforms3d = Modernizr.csstransforms3d;
-
-	if(csstransforms3d === true){
-		$('.img-holder').imageScroll({
-			holderClass: 'parallaxHolder',
-			extraHeight: 200,
-			coverRatio: 0.50,
-			container: $('#main-content'),
-			parallax: csstransforms3d,
-			touch: touch
-		});
-	}
-
-	
-	
+	responsiveImageHelper.bindEvents();	
 });

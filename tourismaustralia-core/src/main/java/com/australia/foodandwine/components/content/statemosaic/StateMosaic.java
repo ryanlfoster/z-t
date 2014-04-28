047f8a7c9cff46d452909a4ba2e17434d60e0e00
Michael Hodgdon <mhodgdon@citytechinc.com>
2014-04-28 11:35:03 +1000

    added text

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/statemosaic/StateMosaic.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/statemosaic/StateMosaic.java
@@ -7,77 +7,79 @@ import org.apache.sling.api.resource.ValueMap;
 import com.australia.utils.LinkUtils;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.FieldProperty;
 import com.citytechinc.cq.component.annotations.Listener;
 import com.citytechinc.cq.component.annotations.Tab;
 import com.citytechinc.cq.component.annotations.widgets.PathField;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 
-@Component(disableTargeting = true, group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", value = "State Mosaic",tabs = {
-	@Tab(title = "Mosaic Header"), @Tab(title = "Register")}, listeners = {
+@Component(disableTargeting = true, group = "Food and Wine", basePath = "jcr_root/apps/foodandwine/components", value = "State Mosaic", tabs = {
+	@Tab(title = "Mosaic Header"), @Tab(title = "Register") }, listeners = {
 	@Listener(name = "aftercopy", value = "REFRESH_PAGE"), @Listener(name = "afterdelete", value = "REFRESH_PAGE"),
 	@Listener(name = "afteredit", value = "REFRESH_PAGE"), @Listener(name = "afterinsert", value = "REFRESH_PAGE") })
 public class StateMosaic {
 
-	@DialogField(fieldLabel = "Top Text",tab=1)
-	private  String topText;
+	@DialogField(fieldLabel = "Top Text", tab = 1)
+	private String topText;
 
-	@DialogField(fieldLabel = "Center Text",tab=1, required=true)
-	private  String centerText;
+	@DialogField(fieldLabel = "Center Text", tab = 1, required = true)
+	private String centerText;
 
-	@DialogField(fieldLabel = "Bottom text",tab=1)
-	private  String bottomText;
-	
-	@DialogField(fieldLabel = "Top Text",tab=2)
-	private  String registerTopText;
+	@DialogField(fieldLabel = "Bottom text", tab = 1)
+	private String bottomText;
 
-	@DialogField(fieldLabel = "Center Text",tab=2 )
-	private  String registerCenterText;
+	@DialogField(fieldLabel = "", tab = 2, xtype = "static", additionalProperties = @FieldProperty(name = "html", value = "<b>This only works on the homepage.  All fields MUST be filled out for this to work.</b><br/>"))
+	private String staticText;
 
-	@DialogField(fieldLabel = "Bottom text",tab=2)
-	private  String registerBottomText;
+	@DialogField(fieldLabel = "Top Text", tab = 2)
+	private String registerTopText;
 
-	@DialogField(fieldLabel = "Button Text ", tab = 2, required=true)
-	private  String buttonText;
+	@DialogField(fieldLabel = "Center Text", tab = 2)
+	private String registerCenterText;
 
-	@DialogField(fieldLabel = "Button Link ", tab = 2,required=true)
+	@DialogField(fieldLabel = "Bottom text", tab = 2)
+	private String registerBottomText;
+
+	@DialogField(fieldLabel = "Button Text ", tab = 2)
+	private String buttonText;
+
+	@DialogField(fieldLabel = "Button Link ", tab = 2)
 	@PathField
-	private  String buttonLink;
+	private String buttonLink;
 
 	@DialogField(fieldLabel = "Text below Button", tab = 2)
-	private  String belowButtonText;
+	private String belowButtonText;
 	private String template;
 
 	private boolean external;
+
 	/**
 	 * Constructor
+	 * 
 	 * @param request
 	 */
 	public StateMosaic(SlingHttpServletRequest request) {
 		// TODO: Load first set server side so it can be indexed by google
 		PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
 		Page currentPage = pageManager.getContainingPage(request.getResource());
-		if(currentPage!=null)
+		if (currentPage != null)
 			template = currentPage.getTemplate().getName();
 		ValueMap properties = request.getResource().adaptTo(ValueMap.class);
-		if(properties!=null)
-		{
+		if (properties != null) {
 			topText = properties.get("topText", StringUtils.EMPTY);
 			centerText = properties.get("centerText", StringUtils.EMPTY);
 			bottomText = properties.get("bottomText", StringUtils.EMPTY);
-			registerTopText=properties.get("registerTopText", StringUtils.EMPTY);
-			registerCenterText=properties.get("registerCenterText", StringUtils.EMPTY);
-			registerBottomText=properties.get("registerBottomText", StringUtils.EMPTY);
-			buttonText=properties.get("buttonText", StringUtils.EMPTY);
-			buttonLink=properties.get("buttonLink", StringUtils.EMPTY);
-			buttonLink=LinkUtils.getHrefFromPath(buttonLink);
-			 external = LinkUtils.isExternal(buttonLink);
-			belowButtonText=properties.get("belowButtonText", StringUtils.EMPTY);
+			registerTopText = properties.get("registerTopText", StringUtils.EMPTY);
+			registerCenterText = properties.get("registerCenterText", StringUtils.EMPTY);
+			registerBottomText = properties.get("registerBottomText", StringUtils.EMPTY);
+			buttonText = properties.get("buttonText", StringUtils.EMPTY);
+			buttonLink = properties.get("buttonLink", StringUtils.EMPTY);
+			buttonLink = LinkUtils.getHrefFromPath(buttonLink);
+			external = LinkUtils.isExternal(buttonLink);
+			belowButtonText = properties.get("belowButtonText", StringUtils.EMPTY);
 		}
 	}
-
-	
-	
 
 	/**
 	 * 
@@ -86,7 +88,7 @@ public class StateMosaic {
 	public String getTemplate() {
 		return template;
 	}
-	
+
 	public String getTopText() {
 		return topText;
 	}
@@ -98,7 +100,7 @@ public class StateMosaic {
 	public String getBottomText() {
 		return bottomText;
 	}
-		
+
 	public String getRegisterTopText() {
 		return registerTopText;
 	}
@@ -122,9 +124,9 @@ public class StateMosaic {
 	public String getBelowButtonText() {
 		return belowButtonText;
 	}
+
 	public boolean isExternal() {
 		return external;
 	}
-
 
 }

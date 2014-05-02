70b16f9bd3cc51d49185faa1c559be6478674707
Michael Hodgdon <mhodgdon@citytechinc.com>
2014-05-02 14:55:33 +1000

    fixed category filter

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/searchresults/domain/FAWSearchParametersBuilder.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/searchresults/domain/FAWSearchParametersBuilder.java
@@ -1,13 +1,9 @@
 package com.australia.foodandwine.searchresults.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.australia.search.domain.AbstractSeachParametersBuilder;
 
 public class FAWSearchParametersBuilder extends AbstractSeachParametersBuilder<FAWSearchParametersBuilder> {
 	private String place;
-	private final List<String> searchFilter = new ArrayList<String>();
 
 	public FAWSearchParametersBuilder setPlace(String place) {
 		this.place = place;
@@ -18,7 +14,6 @@ public class FAWSearchParametersBuilder extends AbstractSeachParametersBuilder<F
 		FAWSearchParameters params = new FAWSearchParameters();
 		super.setParameters(params);
 		params.setPlace(place);
-		params.setTags(searchFilter);
 		return params;
 	}
 }

25a840f11738e3bd35879ead509edfb325981f41
cchin <Christopher.Chin@dt.com.au>
2014-05-06 14:20:26 +1000

    test commit from cfc windows command line git

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/sharethis/ShareThis.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/sharethis/ShareThis.java
@@ -37,7 +37,7 @@ public class ShareThis {
 		Page currentPage = request.getResourceResolver().adaptTo(PageManager.class)
 			.getContainingPage(request.getResource());
 
-		shareType = "current"; // default;
+		shareType = "current"; // default11;
 		shareUrl = currentPage.getPath() + ".html";
 		socialNetworks = this.getSocialNetworks(request).toString();
 	}

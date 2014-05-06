c0d7f2d3e4d017563c4574ea76c911a5bae7a444
Christopher Armstrong <sourcetree@elevenxy.com.au>
2014-05-06 12:21:32 +1000

    [Bug #76598] fixed iPad orientation change for responsive image

--- tourismaustralia-styleguide/assets/js/src/helper/responsive.image.helper.js
+++ tourismaustralia-styleguide/assets/js/src/helper/responsive.image.helper.js
@@ -11,13 +11,15 @@ responsiveImageHelper.bindEvents = function(){
 		window.attachEvent('orientationchange', responsiveImageHelper.setContainerHeight);
 	} else {
 		$(window).resize(function(){
-		// Get screen size (inner/outerWidth, inner/outerHeight)
+		    // Get screen size (inner/outerWidth, inner/outerHeight)
 			responsiveImageHelper.setContainerHeight();
 		});
 
 		window.addEventListener('orientationchange', function() {
-			// Announce the new orientation number
+            // Announce the new orientation number
 			responsiveImageHelper.setContainerHeight();
+            // triggering resize as well because orientationchange on iPad is not reliably re-drawing the height
+            $(window).trigger('resize');
 		}, false);
 	};
 
@@ -31,7 +33,7 @@ responsiveImageHelper.bindEvents = function(){
 };
 
 responsiveImageHelper.setContainerHeight = function(){
-	$('.largeimage-container').each(function(){
+    $('.largeimage-container').each(function(){
 		var offSet = $(this).find('img').height() * parseFloat(20) / 100,
 			maxImageHeight = 600, // Max height of responsive images
 			heightOfImage = $(this).find('img').height() - offSet;

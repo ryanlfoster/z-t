48083b723f6fedbf3521f46fab3a8de46b57a180
gsnyder <gsnyder@citytechinc.com>
2014-05-07 10:37:13 -0500

    76720: Removing redundant 'final' modifier on static method declarations

--- tourismaustralia-core/src/main/java/com/australia/utils/QueryUtils.java
+++ tourismaustralia-core/src/main/java/com/australia/utils/QueryUtils.java
@@ -28,17 +28,17 @@ public class QueryUtils {
 	public static final String AND = ".and";
 	public static final String ASC = "asc";
 
-	public static final void addProperty(Map<String, String> queryMap, int propertyNumber, String property, String value) {
+	public static void addProperty(Map<String, String> queryMap, int propertyNumber, String property, String value) {
 		queryMap.put(propertyNumber + SEPERATOR + PROPERTY, property);
 		queryMap.put(propertyNumber + SEPERATOR + VALUE, value);
 	}
 
-	public static final void addFullText(Map<String, String> queryMap, int propertyNumber, String text) {
+	public static void addFullText(Map<String, String> queryMap, int propertyNumber, String text) {
 		queryMap.put(propertyNumber + SEPERATOR + FULLTEXT, text + "*~");
 		queryMap.put(propertyNumber + SEPERATOR + FULLTEXT + RELPATH, NameConstants.NN_CONTENT);
 	}
 
-	public static final void addQueryForTags(Map<String, String> queryMap, List<Tag> tags, int propertyNumber) {
+	public static void addQueryForTags(Map<String, String> queryMap, List<Tag> tags, int propertyNumber) {
 		String tagPrefix = propertyNumber + SEPERATOR + TAGID;
 		queryMap.put(tagPrefix + ".property", TAG_LOCATION);
 		int valueCount = 1;
@@ -48,7 +48,7 @@ public class QueryUtils {
 		}
 	}
 
-	public static final void addProperty(final Map<String, String> queryMap, final int propertyNumber,
+	public static void addProperty(final Map<String, String> queryMap, final int propertyNumber,
 		final String property, final List<String> values) {
 		final String prefix = propertyNumber + SEPERATOR + PROPERTY;
 		queryMap.put(prefix, property);

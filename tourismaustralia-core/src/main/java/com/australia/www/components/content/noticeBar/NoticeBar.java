87dbb602b0b4c5648840485e96d8f324402671e7
wynnem <wynne.ma@dt.com>
2014-05-12 10:23:25 +1000

    WM: Reformatted Code

--- tourismaustralia-core/src/main/java/com/australia/www/components/content/noticeBar/NoticeBar.java
+++ tourismaustralia-core/src/main/java/com/australia/www/components/content/noticeBar/NoticeBar.java
@@ -14,52 +14,55 @@ import org.apache.sling.api.resource.ValueMap;
  * Created by wma on 8/05/2014.
  */
 
-@Component(value = "Notice Bar",  dialogWidth = 500, disableTargeting = true, group = ".hidden", listeners = { @Listener(name = "aftercopy", value = "REFRESH_PAGE"), @Listener(name = "afterdelete", value = "REFRESH_PAGE"), @Listener(name = "afteredit", value = "REFRESH_PAGE"), @Listener(name = "afterinsert", value = "REFRESH_PAGE") })
+@Component(value = "Notice Bar", dialogWidth = 500, disableTargeting = true, group = ".hidden",
+	listeners = { @Listener(name = "aftercopy", value = "REFRESH_PAGE"),
+		@Listener(name = "afterdelete", value = "REFRESH_PAGE"), @Listener(name = "afteredit", value = "REFRESH_PAGE"),
+		@Listener(name = "afterinsert", value = "REFRESH_PAGE") })
 
 public class NoticeBar {
-    @DialogField(fieldLabel = "Title", required = false)
-    private String title;
+	@DialogField(fieldLabel = "Title", required = false)
+	private String title;
 
-    @DialogField(fieldLabel = "Summary", required = false)
-    private String text;
+	@DialogField(fieldLabel = "Summary", required = false)
+	private String text;
 
-    @DialogField(fieldLabel = "Link text", required = false)
-    private String linkText;
+	@DialogField(fieldLabel = "Link text", required = false)
+	private String linkText;
 
-    @DialogField(fieldLabel = "Link Path", required = false)
-    @PathField(rootPath = "/content")
-    private String linkPath;
+	@DialogField(fieldLabel = "Link Path", required = false)
+	@PathField(rootPath = "/content")
+	private String linkPath;
 
-    private boolean isExternal;
+	private boolean isExternal;
 
-    public NoticeBar(SlingHttpServletRequest request) {
-        ValueMap properties = request.getResource().adaptTo(ValueMap.class);
-        if (properties != null) {
-            title = properties.get("title", StringUtils.EMPTY);
-            text = properties.get("text", StringUtils.EMPTY);
-            linkText = properties.get("linkText", StringUtils.EMPTY);
-            linkPath = LinkUtils.getHrefFromPath(properties.get("linkPath", StringUtils.EMPTY));
-            isExternal = LinkUtils.isExternal(properties.get("linkPath", StringUtils.EMPTY));
-        }
-    }
+	public NoticeBar(SlingHttpServletRequest request) {
+		ValueMap properties = request.getResource().adaptTo(ValueMap.class);
+		if (properties != null) {
+			title = properties.get("title", StringUtils.EMPTY);
+			text = properties.get("text", StringUtils.EMPTY);
+			linkText = properties.get("linkText", StringUtils.EMPTY);
+			linkPath = LinkUtils.getHrefFromPath(properties.get("linkPath", StringUtils.EMPTY));
+			isExternal = LinkUtils.isExternal(properties.get("linkPath", StringUtils.EMPTY));
+		}
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getText() {
-        return text;
-    }
+	public String getText() {
+		return text;
+	}
 
-    public String getLinkText() {
-        return linkText;
-    }
+	public String getLinkText() {
+		return linkText;
+	}
 
-    public String getLinkPath() {
-        return linkPath;
-    }
+	public String getLinkPath() {
+		return linkPath;
+	}
 
-    public boolean getIsExternal() {
-        return isExternal;
-    }
+	public boolean getIsExternal() {
+		return isExternal;
+	}
 }
--- tourismaustralia-core/src/main/java/com/australia/www/link/TextLinkWithState.java
+++ tourismaustralia-core/src/main/java/com/australia/www/link/TextLinkWithState.java
@@ -11,51 +11,56 @@ import com.citytechinc.cq.component.annotations.widgets.Selection;
  * Created by wma on 2/05/2014.
  */
 public class TextLinkWithState {
-    @DialogField(fieldLabel = "Title", required = true, additionalProperties = @FieldProperty(name = "anchor", value = "100%"))
-    private String linkText;
+	@DialogField(fieldLabel = "Title", required = true,
+		additionalProperties = @FieldProperty(name = "anchor", value = "100%"))
+	private String linkText;
 
-    @DialogField(fieldLabel = "Link Path", required = true, additionalProperties = @FieldProperty(name = "anchor", value = "100%"))
-    @PathField(rootPath = "/content")
-    private String pagePath;
+	@DialogField(fieldLabel = "Link Path", required = true,
+		additionalProperties = @FieldProperty(name = "anchor", value = "100%"))
+	@PathField(rootPath = "/content")
+	private String pagePath;
 
-    @DialogField(fieldLabel = "State", required = true)
-    @Selection(type = Selection.SELECT, options = { @Option(value = "vic", text = "Victoria"), @Option(value = "nsw", text = "New South Wales"),
-            @Option(value = "qld", text = "Queensland"), @Option(value = "sa", text = "South Australia"), @Option(value = "nt", text = "Northern Territory"),
-            @Option(value = "wa", text = "Western Australia"), @Option(value = "act", text = "Australian Capital Territory"),
-            @Option(value = "tas", text = "Tasmania") })
-    private String state;
+	@DialogField(fieldLabel = "State", required = true)
+	@Selection(type = Selection.SELECT,
+		options = { @Option(value = "vic", text = "Victoria"), @Option(value = "nsw", text = "New South Wales"),
+			@Option(value = "qld", text = "Queensland"), @Option(value = "sa", text = "South Australia"),
+			@Option(value = "nt", text = "Northern Territory"),
+			@Option(value = "wa", text = "Western Australia"),
+			@Option(value = "act", text = "Australian Capital Territory"),
+			@Option(value = "tas", text = "Tasmania") })
+	private String state;
 
-    private boolean isExternal;
+	private boolean isExternal;
 
-    public String getLinkText() {
-        return linkText;
-    }
+	public String getLinkText() {
+		return linkText;
+	}
 
-    public void setLinkText(String linkText) {
-        this.linkText = linkText;
-    }
+	public void setLinkText(String linkText) {
+		this.linkText = linkText;
+	}
 
-    public String getPagePath() {
-        return pagePath;
-    }
+	public String getPagePath() {
+		return pagePath;
+	}
 
-    public void setPagePath(String pagePath) {
-        this.pagePath = pagePath;
-    }
+	public void setPagePath(String pagePath) {
+		this.pagePath = pagePath;
+	}
 
-    public String getState() {
-        return state;
-    }
+	public String getState() {
+		return state;
+	}
 
-    public void setState(String state) {
-        this.state = state;
-    }
+	public void setState(String state) {
+		this.state = state;
+	}
 
-    public boolean getIsExternal() {
-        return isExternal;
-    }
+	public boolean getIsExternal() {
+		return isExternal;
+	}
 
-    public void setIsExternal(boolean isExternal) {
-        this.isExternal = isExternal;
-    }
+	public void setIsExternal(boolean isExternal) {
+		this.isExternal = isExternal;
+	}
 }

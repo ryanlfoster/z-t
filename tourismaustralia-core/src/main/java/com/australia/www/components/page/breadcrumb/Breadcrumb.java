b6ab77b002ef220c8a5b5bebf33483ca52b31bb8
cchin <Christopher.Chin@dt.com.au>
2014-05-12 17:10:02 +1000

    Component refactored

--- tourismaustralia-core/src/main/java/com/australia/www/components/page/breadcrumb/Breadcrumb.java
+++ tourismaustralia-core/src/main/java/com/australia/www/components/page/breadcrumb/Breadcrumb.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 
-import com.australia.utils.PathUtils;
-import com.australia.utils.ServerUtils;
 import com.citytechinc.cq.component.annotations.Component;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
@@ -16,57 +13,28 @@ import com.day.cq.wcm.api.PageManager;
 @Component(value = "Breadcrumb", actions = { "text:Breadcrumb", "-", "copymove", "delete", "-", "insert" })
 public class Breadcrumb {
 
+	private final Iterator<Page> predecessorPages;
+
 	private final Page currentPage;
-
-	private boolean localeRootPage;
-
-	private final boolean homePage4Aus;
 
 	public Breadcrumb(SlingHttpServletRequest slingRequest) {
 		currentPage = slingRequest.getResourceResolver().adaptTo(PageManager.class)
 			.getContainingPage(slingRequest.getResource());
-		localeRootPage = setLocalePage(slingRequest);
-		homePage4Aus = currentPage.getPath().equals(PathUtils.OZCOM_ROOT_PATH);
+		predecessorPages = buildPagesList();
 	}
 
-	private boolean setLocalePage(SlingHttpServletRequest slingRequest) {
-		String lang = ServerUtils.getLanguageCode(slingRequest);
-		localeRootPage = (StringUtils.isNotEmpty(lang)) && (currentPage.getDepth() > 2)
-			&& (currentPage.getAbsoluteParent(2).getPath().endsWith(lang));
-		return localeRootPage;
+	private Iterator<Page> buildPagesList() {
+		int localLevel = 3;
+		List<Page> resultlist = new ArrayList<Page>();
+		while ((currentPage.getDepth() > localLevel) && (localLevel <= (currentPage.getParent().getDepth()))) {
+			resultlist.add(currentPage.getAbsoluteParent(localLevel - 1));
+			localLevel++;
+		}
+		return resultlist.listIterator();
 	}
 
 	public Iterator<Page> getPredecessorPages() {
-		Iterator<Page> predecessorListIterator = null;
-		if (localeRootPage) {
-			predecessorListIterator = buildPagesList(3);
-		} else {
-			predecessorListIterator = buildPagesList(2);
-		}
-		return predecessorListIterator;
-	}
-
-	private Iterator<Page> buildPagesList(int localLevel) {
-		List<Page> predecessorList = new ArrayList<Page>();
-		int startIndex = 0;
-		while ((currentPage.getDepth() > localLevel) && (localLevel <= (currentPage.getParent().getDepth()))) {
-			predecessorList.add(startIndex, currentPage.getAbsoluteParent(localLevel - 1));
-			localLevel++;
-			startIndex++;
-		}
-		return predecessorList.listIterator();
-	}
-
-	public Page getCurrentPage() {
-		return currentPage;
-	}
-
-	public boolean isLocaleRootPage() {
-		return localeRootPage;
-	}
-
-	public boolean isHomePage4Aus() {
-		return homePage4Aus;
+		return predecessorPages;
 	}
 
 }

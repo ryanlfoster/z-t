f33fa4b34138ebfb67c8f0cca2f3b659149fa81a
Singh, Sapandeep <sapandeep.singh@dt.com.au>
2014-05-13 17:17:33 +1000

    NavScroller - Update Side Links according to section watching

--- tourismaustralia-styleguide/assets/js/src/helper/navScroller.js
+++ tourismaustralia-styleguide/assets/js/src/helper/navScroller.js
@@ -1,12 +1,16 @@
 var navScroller = navScroller || {};
 
 navScroller.sectionDistances = {};
+navScroller.sectionLinks = new Array();
 
 navScroller.init = function(){
 
-	// store distances from top for each section.	
+	// store distances from top for each section.
 	$('.navScroller-section').each(function(index, value) {
 		navScroller.sectionDistances[index] = $(this).offset().top;
+
+        var hashLink = $(this).attr('data-section-name');
+        navScroller.sectionLinks.push($('ul.navScroller a[href="#'+ hashLink +'"]'));
 	});
 
 	navScroller._bindEvents();
@@ -24,6 +28,7 @@ navScroller._bindEvents = function(){
 			target = target.length ? target : $('[data-section-name=' + this.hash.slice(1) +']');
 
 			if (target.length) {
+
 				$('html,body').animate({
 				  scrollTop: target.offset().top - ($('.main-nav-panel').height() - 1) // offset to accommodate navbar
 				}, 1000);
@@ -37,12 +42,62 @@ navScroller._bindEvents = function(){
 			}
 		}
 	});
+
+
+
 };
 
 // check if section is in view
 navScroller._updateActive = function(){
 
+    var distance1, distance2;
+    var hashLink;
+    var totalNo_navScroller_section=$('.navScroller-section').size();
+    $window = $(window);
+
+
+    $window.scroll(function() {
+        $('.navScroller-section').each(function(index){
+
+            //Top of current section
+            distance1 = navScroller.sectionDistances[index];
+
+            if(index < totalNo_navScroller_section-1)
+            {
+                //Top of next to current section
+                distance2 = navScroller.sectionDistances[index + 1];
+
+                if ( $window.scrollTop() >= distance1 && $window.scrollTop() < distance2 )
+                {
+                    ResetNavScroller();
+
+                    //Set active to link in 'navScroller' according to current section
+                    $(navScroller.sectionLinks[index]).parent().addClass('active');
+                }
+            }
+            else{
+                console.log($window.scrollTop());
+                console.log(distance1);
+                console.log('----------------');
+                if ( $window.scrollTop() >= distance1)
+                {
+                    ResetNavScroller();
+
+                    //Set active to link in 'navScroller' according to current section
+                    $(navScroller.sectionLinks[index]).parent().addClass('active');
+                }
+            }
+        });
+    });
 };
+
+//Reset all links of 'navScroller'
+function ResetNavScroller()
+{
+    $('ul.navScroller li').each(function() {
+        $(this).removeClass('active');
+    });
+}
 
 $(function() {
 	navScroller.init();	

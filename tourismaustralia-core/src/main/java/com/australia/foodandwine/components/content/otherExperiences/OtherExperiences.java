9754c03c87f7f789d28fdf67422e797bcd54f9bf
Ashwin Gurumurthy <agurumurthy@citytechinc.com>
2014-05-22 16:29:17 +0530

    fixed automatic rendering of experience articles

--- tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/otherExperiences/OtherExperiences.java
+++ tourismaustralia-core/src/main/java/com/australia/foodandwine/components/content/otherExperiences/OtherExperiences.java
@@ -3,6 +3,14 @@ package com.australia.foodandwine.components.content.otherExperiences;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
+import javax.jcr.query.Row;
+import javax.jcr.query.RowIterator;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
@@ -11,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.australia.utils.LinkUtils;
+import com.australia.utils.PathUtils;
 import com.australia.utils.TagUtils;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
@@ -47,14 +56,19 @@ public class OtherExperiences {
 
 	private final List<OtherExperiencesArticleListProperties> articlesList = new ArrayList<OtherExperiencesArticleListProperties>();
 	private List<String> experienceList;
+	private static final String QUERY_STRING = "SELECT * FROM [cq:Page] AS page INNER JOIN [nt:unstructured] AS pageContent ON ISCHILDNODE(pageContent,page) WHERE ISDESCENDANTNODE(page,["
+		+ PathUtils.FOOD_AND_WINE_ROOT_PATH
+		+ "]) and pageContent.[cq:tags] like '%s%%' and not (issamenode(page,'%s') or pageContent.excludeFromMosaic = true) and pageContent.[cq:template] = '/apps/foodandwine/templates/articlepage' order by pageContent.[jcr:created] DESC";
+
 	private String stateTitle;
 
 	public OtherExperiences(SlingHttpServletRequest request) {
 		ValueMap linkProps = request.getResource().adaptTo(ValueMap.class);
 		experienceList = new ArrayList<String>();
-
+		PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
+		Page currentPage = pageManager.getContainingPage(request.getResource());
+		TagManager tagManager = request.getResourceResolver().adaptTo(TagManager.class);
 		try {
-
 			String otherExperiencesPagePath1 = linkProps.get("otherExperiencesPagePath1", String.class);
 			String otherExperiencesPagePath2 = linkProps.get("otherExperiencesPagePath2", String.class);
 			String otherExperiencesPagePath3 = linkProps.get("otherExperiencesPagePath3", String.class);
@@ -65,20 +79,37 @@ public class OtherExperiences {
 				experienceList.add(otherExperiencesPagePath2);
 				experienceList.add(otherExperiencesPagePath3);
 				experienceList.add(otherExperiencesPagePath4);
-
 				for (String experiencePath : experienceList) {
 					if (experiencePath.contains(".html")) {
 						experiencePath = experiencePath.replace(".html", StringUtils.EMPTY);
 					}
-					PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
-					Page currentPage = pageManager.getContainingPage(request.getResource());
-					String[] pageTags = currentPage.getProperties().get("cq:tags", new String[0]);
-					TagManager tagManager = request.getResourceResolver().adaptTo(TagManager.class);
-					Tag stateTag = TagUtils.getStateTag(tagManager, pageTags);
-					if (stateTag != null) {
-						stateTitle = stateTag.getTitle();
-					}
-					Page articlePage = pageManager.getPage(experiencePath);
+					generateExperiences(request, experiencePath, pageManager, currentPage, tagManager);
+				}
+			} else {
+				queryBasedExperienceDisplay(request, pageManager, currentPage, tagManager);
+			}
+		} catch (Exception e) {
+			LOG.error(e.getMessage());
+		}
+	}
+
+	private void queryBasedExperienceDisplay(SlingHttpServletRequest request, PageManager pageManager,
+		Page currentPage, TagManager tagManager) {
+		String[] pageTags = currentPage.getProperties().get("cq:tags", new String[0]);
+		Tag stateTag = TagUtils.getStateTag(tagManager, pageTags);
+		if (stateTag != null) {
+			stateTitle = stateTag.getTitle();
+			Session session = request.getResourceResolver().adaptTo(Session.class);
+			try {
+				QueryManager queryManager = session.getWorkspace().getQueryManager();
+				Query query = queryManager.createQuery(
+					String.format(QUERY_STRING, stateTag.getTagID(), currentPage.getPath()), Query.JCR_SQL2);
+				query.setLimit(4);
+				QueryResult result = query.execute();
+				RowIterator rowIterator = result.getRows();
+				while (rowIterator != null && rowIterator.hasNext()) {
+					Row row = rowIterator.nextRow();
+					Page articlePage = pageManager.getPage(row.getPath("page"));
 					ValueMap pageProperties = articlePage.getProperties();
 					String title = articlePage.getTitle();
 					String image = null;
@@ -97,20 +128,51 @@ public class OtherExperiences {
 					if (cityTag != null) {
 						city = cityTag.getTitle();
 					}
-					String state = StringUtils.EMPTY;
-					if (stateTag != null) {
-						state = stateTag.getTitle();
-					}
+					String state = stateTag.getTitle();
 					String icon = pageProperties.get("categoryLogoPath", String.class);
 					articlesList.add(new OtherExperiencesArticleListProperties(title, LinkUtils
 						.getHrefFromPath(articlePage.getPath()), image, city, state, tagTitles.toArray(new String[0]),
 						icon));
 				}
+			} catch (RepositoryException e) {
+				LOG.error("Error using query manager", e);
 			}
-		} catch (Exception e) {
-			LOG.error(e.getMessage());
 		}
+	}
 
+	private void generateExperiences(SlingHttpServletRequest request, String experiencePath, PageManager pageManager,
+		Page currentPage, TagManager tagManager) {
+		String[] pageTags = currentPage.getProperties().get("cq:tags", new String[0]);
+		Tag stateTag = TagUtils.getStateTag(tagManager, pageTags);
+		if (stateTag != null) {
+			stateTitle = stateTag.getTitle();
+		}
+		Page articlePage = pageManager.getPage(experiencePath);
+		ValueMap pageProperties = articlePage.getProperties();
+		String title = articlePage.getTitle();
+		String image = null;
+		Resource jcrResource = articlePage.adaptTo(Resource.class).getChild(JcrConstants.JCR_CONTENT);
+		Image pageImage = new Image(jcrResource, "image");
+		if (pageImage != null && pageImage.hasContent()) {
+			image = pageImage.getPath() + ".img.jpg";
+		}
+		String[] tags = pageProperties.get("cq:tags", new String[0]);
+		List<String> tagTitles = new ArrayList<String>();
+		for (Tag tag : TagUtils.getFoodAndWineCategoryTags(tagManager, tags)) {
+			tagTitles.add(tag.getTitle());
+		}
+		String city = null;
+		Tag cityTag = TagUtils.getCityTag(tagManager, tags);
+		if (cityTag != null) {
+			city = cityTag.getTitle();
+		}
+		String state = StringUtils.EMPTY;
+		if (stateTag != null) {
+			state = stateTag.getTitle();
+		}
+		String icon = pageProperties.get("categoryLogoPath", String.class);
+		articlesList.add(new OtherExperiencesArticleListProperties(title, LinkUtils.getHrefFromPath(articlePage
+			.getPath()), image, city, state, tagTitles.toArray(new String[0]), icon));
 	}
 
 	public List<OtherExperiencesArticleListProperties> getArticlesList() {
